a1055eebfc8187a8fc42e9e1c44c840338a9a01d
Ishaan Singh <asishaan.191cs124@nitk.edu.in>
2021-03-21 19:01:51 +0530

    bbbs

---
+++ project_files/Code/mux_w5_0.v
@@ -0,0 +1,13 @@
+module mux_w5_0(S,I0,I1,out);
+	input S, I0, I1;
+	output [8:0] out;
+	assign out[0] = I0;
+	assign out[1] = I1;
+	assign out[2] = S;
+	assign out[3] = S;
+	assign out[4] = S;
+	assign out[5] = 0;
+	assign out[6] = I1 & S;
+	assign out[7] = I0 & (0);
+	assign out[8] = (I1 & S) | (I0 & (0));
+endmodule
--- project_files/tb.v
+++
@@ -1,9 +0,0 @@
-module tb;
-	test t(a, b);
-	reg [5:0] a;
-	wire [5:0] b;
-	initial begin
-		#0 a = 10;
-		$monitor("b : %b", b);
-	end
-endmodule
